1c2f8f4df553a4b646bc8aacdb4f577eda21e43d
chanchishing <chishing@netvigator.com>
2022-01-10 14:37:23 +0800

    Project Creation

---
+++ src/main/java/guru/springframework/sping5jokesapp/Sping5JokesAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sping5jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sping5JokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sping5JokesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sping5jokesapp/Sping5JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sping5jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sping5JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
